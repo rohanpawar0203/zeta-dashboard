6d75928fb288ca41eab4a372e235d46bae726ddc
rohanpawar0203 <103638389+rohanpawar0203@users.noreply.github.com>
2024-01-24 12:04:39 +0530

    Analytics added and ui changes

---
+++ src/Component/Dash-board/chartData.js
@@ -0,0 +1,40 @@
+import axios from "axios";
+
+export const apiCall = async (userId, params) => {
+  try {
+    let token = sessionStorage.getItem("token");
+    let config = {
+      method: "get",
+      maxBodyLength: Infinity,
+      url: `https://ulai.in/backend/analytics/${userId}/${params}/data?days=30`,
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+    const response = await axios.request(config);
+    return response.data.data;
+    // if (newData.length !== 0) {
+    //   const newOptions = newData.map((element) =>
+    //     new Date(element._id).getDate()
+    //   );
+    //   setChatOptions((prevOptions) => ({
+    //     ...prevOptions,
+    //     xaxis: {
+    //       ...prevOptions.xaxis,
+    //       categories: [...prevOptions.xaxis.categories, ...newOptions],
+    //     },
+    //   }));
+
+    //   const newSeries = newData.map((element) => element.count);
+    //   setChatSeries((prevSeries) => [
+    //     {
+    //       ...prevSeries[0],
+    //       data: [...prevSeries[0].data, ...newSeries],
+    //     },
+    //   ]);
+    // }
+  } catch (error) {
+    console.log("chat-session api", error);
+    return error;
+  }
+};
