9669d9343e6bb260e39e90dd2e9f945b3769db30
premkumar <premkumar@paraslabs.com>
2024-03-04 18:47:31 +0530

    added routes controllers for agent on agent login

--- src/_helper/MenuItems/MenuItemsProvider.jsx
+++ src/_helper/MenuItems/MenuItemsProvider.jsx
@@ -30,10 +30,21 @@ export const MenuItemsContextProvider = ({ children }) => {
     }
    
     const handleFilterForAgent = () => {
-        let replacedItems = menuItemsRef.current.filter((ele) => (ele.menutitle === 'Live Chats'));
+        let replacedItems = menuItemsRef.current.filter((ele) => {
+          let allowedRoutes = ['Live Chats', 'Tickets', 'Dashboard', 'Chats Panel', 'Request a feature'];
+          if(allowedRoutes.indexOf(ele?.menutitle) !== -1){
+            return ele;
+          }
+        });
         setData(replacedItems);
 
-        let filteredRoutes = routeItemsRef.current.filter((ele) => (ele.path === `${process.env.PUBLIC_URL}/live-chat`))
+        let filteredRoutes = routeItemsRef.current.filter((ele) => {
+          let allowedRoutes = ['live-chat', 'dashboard', 'chats-panel', 'tickets'];
+          let endPoint = ele?.path?.split('/')?.pop();
+          if(allowedRoutes.indexOf(endPoint) !== -1){
+            return true;
+          }
+        });
         setRoutesData([...filteredRoutes]);
     }
 
